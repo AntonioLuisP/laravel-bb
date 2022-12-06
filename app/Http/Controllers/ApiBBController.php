31d38590fba51ea73a158bf2a083d86a73a77eca
Antonio <antoniojunior11@aluno.uema.br>
2022-12-06 15:03:51 -0300

    quase ok

--- app/Http/Controllers/ApiBBController.php
+++ app/Http/Controllers/ApiBBController.php
@@ -2,13 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
 use GuzzleHttp\Exception\GuzzleException;
 
 class ApiBBController extends Controller
 {
-    public function token(Request $request)
+    public function token()
     {
         try {
             $guzzle = new Client([
@@ -33,7 +33,7 @@ class ApiBBController extends Controller
             $contents = $body->getContents();
 
             $token = json_decode($contents);
-            dd($token->access_token);
+            return $token->access_token;
         } catch (GuzzleException $e) {
             echo $e->getMessage();
         }
@@ -41,17 +41,133 @@ class ApiBBController extends Controller
 
     public function registrar()
     {
-        dd('registrar');
+        $body = [
+            'numeroConvenio' => '',
+            'numeroCarteira' => '',
+            'numeroVariacaoCarteira' => '',
+            'codigoModalidade' => '',
+            'dataEmissao' => '',
+            'dataVencimento' => '',
+            'valorOriginal' => '',
+            'valorAbatimento' => '',
+            'quantidadeDiasProtesto',
+            'indicadorNumeroDiasLimiteRecebimento' => '',
+            'numeroDiasLimiteRecebimento' => '',
+            'codigoAceite'  => 'A',
+            'codigoTipoTitulo' => 'DS',
+            'descricaoipoTitulo' => '',
+            'indicadorPermissaoRecebimentoParcial' => 'N',
+            'numeroTituloBeneficiario' => '',
+            'textoCampoUtiliacaoBeneficiario' => 'TESTE',
+            'codigoTipoContaCaucao' => '',
+            'numeroTituloCliente' => '',
+            'textoMensagemBloquetoOcorrencia' => 'TESTE',
+            'pagador' => [
+                'tipoRegistro' => '',
+                'numeroRegistro' => '',
+                'nome' => 'TESTE',
+                'endereco' => '',
+                'cep' => '',
+                'cidade' => '',
+                'bairro' => '',
+                'uf' => '',
+                'telefone' => ''
+            ],
+            'email' => ''
+        ];
+
+        $body = json_encode($body);
+
+        try {
+            $guzzle = new Client([
+                'headers' => [
+                    'Authorization' => 'Bearer ' . $this->token(),
+                    'Content-Type' => 'application/json'
+                ],
+                'verify' => false
+            ]);
+
+            $response = $guzzle->request('POST', 'https://api.hm.bb.com.br/cobrancas/v1/boletos?gw_dev_app_key=' . config('apiCobranca.gw_dev_app_key'), [
+                'body' => $body
+            ]);
+
+            $body = $response->getBody();
+
+            $contents = $body->getContents();
+
+            $boleto = json_decode($contents);
+
+            dd($boleto);
+        } catch (ClientException $e) {
+            echo $e->getMessage();
+        }
     }
 
     public function listar()
     {
-        dd('listar');
+        try {
+            $guzzle = new Client([
+                'headers' => [
+                    'Authorization' => 'Bearer ' . $this->token(),
+                    'Content-Type' => 'application/json'
+                ],
+                'verify' => false
+            ]);
+
+            $response = $guzzle->request(
+                'POST',
+                'https://api.hm.bb.com.br/cobrancas/v1/boletos?gw_dev_app_key=' . config('apiCobranca.gw_dev_app_key') .
+                    '&agenciaBenefciario=' . '452' .
+                    '&contaBeneficiario=' . '123873' .
+                    '&indicadorSituacao=' . 'B' .
+                    '&indice=' . '300' .
+                    '&codigoEstadoTituloCobranca=' . '7' .
+                    '&dataInicioMovimento=' . '01.01.2021' .
+                    '&dataFimMovimento=' . '01.01.2021'
+            );
+
+            $body = $response->getBody();
+
+            $contents = $body->getContents();
+
+            $boleto = json_decode($contents);
+
+            dd($boleto);
+        } catch (GuzzleException $e) {
+            echo $e->getMessage();
+        }
     }
 
     public function consultar()
     {
-        dd('consultar');
+        $id = '';
+        try {
+            $guzzle = new Client([
+                'headers' => [
+                    'Authorization' => 'Bearer ' . $this->token(),
+                    'Content-Type' => 'application/json'
+                ],
+                'verify' => false
+            ]);
+
+            $response = $guzzle->request(
+                'POST',
+                'https://api.hm.bb.com.br/cobrancas/v1/boletos/' .
+                    $id .
+                    '?gw_dev_app_key=' . config('apiCobranca.gw_dev_app_key') .
+                    '&numeroConvenio=' . '123873'
+            );
+
+            $body = $response->getBody();
+
+            $contents = $body->getContents();
+
+            $boleto = json_decode($contents);
+
+            dd($boleto);
+        } catch (GuzzleException $e) {
+            echo $e->getMessage();
+        }
     }
 
     public function baixar()
@@ -61,6 +177,43 @@ class ApiBBController extends Controller
 
     public function atualizar()
     {
-        dd('atualizar');
+        $id = '';
+
+        $dados = [
+            'numeroConvenio' => '',
+            'indicadorNovaDataVencimento' => '',
+            'alteracaoData' => [
+                'novaDataVencimento' => '13.02.2021'
+            ]
+        ];
+
+        $dados = json_encode($dados);
+
+        try {
+            $guzzle = new Client([
+                'headers' => [
+                    'Authorization' => 'Bearer ' . $this->token(),
+                    'Content-Type' => 'application/json'
+                ],
+                'verify' => false
+            ]);
+
+            $response = $guzzle->request(
+                'PATCH',
+                'https://api.hm.bb.com.br/cobrancas/v1/boletos/' . $id . '?gw_dev_app_key=' . config('apiCobranca.gw_dev_app_key'), [
+                    'body' => $dados
+                ]
+            );
+
+            $body = $response->getBody();
+
+            $contents = $body->getContents();
+
+            $boleto = json_decode($contents);
+
+            dd($boleto);
+        } catch (GuzzleException $e) {
+            echo $e->getMessage();
+        }
     }
 }
